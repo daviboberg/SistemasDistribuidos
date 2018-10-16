9ed850489869d722e4f38a1d1ca1a254d0a58677
Davi Boberg <daviboberg@gmail.com>
2018-10-15 23:41:16 -0300

    Interaction with client

--- Sugarfoot/src/Comunication/InterfaceClient.java
+++ Sugarfoot/src/Comunication/InterfaceClient.java
@@ -1,9 +1,15 @@
 package Comunication;
 
+import Resources.Resource;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 
 public interface InterfaceClient extends Remote {
     void echo(String str) throws RemoteException;
+
+    void getInformation(Resource resource) throws RemoteException;
+
+    void postOrder(Resource resource) throws RemoteException;
 }
