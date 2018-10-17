b9904b3cf0750c45bf459e092ee7d53cf602346a
Davi Boberg <daviboberg@gmail.com>
2018-10-16 22:54:19 -0300

    Update resources and database

--- Sugarfoot/src/Resources/Airplane.java
+++ Sugarfoot/src/Resources/Airplane.java
@@ -2,7 +2,6 @@ package Resources;
 
 import Server.DatabaseConnection;
 
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,6 +11,21 @@ import java.util.List;
 public class Airplane implements Resource {
     public int id;
     public String flight_number;
+    public String origin;
+    public String destiny;
+    public String flight_date;
+    public int passengers;
+
+    public Airplane(String flight_number, String origin, String destiny, String flight_date, int passengers) {
+        this.flight_number = flight_number;
+        this.origin = origin;
+        this.destiny = destiny;
+        this.flight_date = flight_date;
+        this.passengers = passengers;
+    }
+
+    public Airplane() {
+    }
 
     @Override
     public boolean equals(Resource resource) {
@@ -25,10 +39,13 @@ public class Airplane implements Resource {
 
     @Override
     public void insert() {
-        String query = "INSERT INTO airplane (`flight_number`) VALUES (?)";
+        String query = "INSERT INTO airplane (`flight_number`, `origin`, `destiny`, `flight_date`) VALUES (?, ?, ?, ?)";
         PreparedStatement statement = DatabaseConnection.getStatement(query);
         try {
             statement.setString(1, this.flight_number);
+            statement.setString(2, this.origin);
+            statement.setString(3, this.destiny);
+            statement.setString(4, this.flight_date);
             statement.execute();
             this.id = DatabaseConnection.getLastInsertedId();
         } catch (SQLException e) {
@@ -62,21 +79,23 @@ public class Airplane implements Resource {
         }
     }
 
-    public List<AirplaneVO> findAll(String origin, String destiny, Date flight_date) throws SQLException {
-        String query = "SELECT * FROM airplane WHERE origin = ? AND destiny = ? AND flight_date = ?";
+    public List<Airplane> findAvailable() throws SQLException {
+        String query = "SELECT * FROM airplane WHERE origin = ? AND destiny = ? AND flight_date = ? AND available_seats >= ?";
         PreparedStatement statement = DatabaseConnection.getStatement(query);
+        assert statement != null;
         statement.setString(1, origin);
         statement.setString(2, destiny);
-        statement.setDate(3, flight_date);
+        statement.setString(3, flight_date);
+        statement.setInt(4, passengers);
         ResultSet result = statement.executeQuery();
-        List<AirplaneVO> airplanes = new ArrayList<>();
+        List<Airplane> airplanes = new ArrayList<>();
         while (result.next()) {
             int id = result.getInt("id");
             String flight_number = result.getString("flight_number");
             origin = result.getString("origin");
             destiny = result.getString("destiny");
-            flight_date = result.getDate("flight_date");
-            AirplaneVO newAirplane = new AirplaneVO(id, flight_number, origin, destiny, flight_date);
+            flight_date = result.getString("flight_date");
+            Airplane newAirplane = new Airplane(flight_number, origin, destiny, flight_date, passengers);
             airplanes.add(newAirplane);
         }
         return airplanes;
--- Sugarfoot/src/Resources/Hotel.java
+++ Sugarfoot/src/Resources/Hotel.java
@@ -8,6 +8,18 @@ import java.sql.SQLException;
 public class Hotel implements Resource{
     public int id;
     public String name;
+    public int room_number;
+    public String location;
+    public int capacity;
+    public String available_initial_date;
+    public String available_end_date;
+    public float price;
+
+    public Hotel(String location, String available_initial_date, String available_end_date) {
+        this.location = location;
+        this.available_initial_date = available_initial_date;
+        this.available_end_date = available_end_date;
+    }
 
     @Override
     public boolean equals(Resource resource) {
@@ -21,10 +33,17 @@ public class Hotel implements Resource{
 
     @Override
     public void insert() {
-        String query = "INSERT INTO hotel (`name`) VALUES (?)";
+        String query = "INSERT INTO hotel (`name`, `room_number`, `location`, `capacity`, `available_initial_date`, `available_end_date`, `price`) VALUES (?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement statement = DatabaseConnection.getStatement(query);
         try {
             statement.setString(1, this.name);
+            statement.setInt(2, this.room_number);
+            statement.setString(3, this.location);
+            statement.setString(4, this.name);
+            statement.setInt(5, this.capacity);
+            statement.setString(6, this.available_initial_date);
+            statement.setString(7, this.available_end_date);
+            statement.setFloat(6, this.price);
             statement.execute();
             this.id = DatabaseConnection.getLastInsertedId();
         } catch (SQLException e) {
